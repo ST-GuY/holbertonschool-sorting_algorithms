be1eee6b51ac72b30d5b01f64dcbc7525c3304f9
ST-GuY <estebanlvy23@gmail.com>
2025-08-07 17:00:14 +0200

    selection sort

---
+++ 2-selection_sort.c
@@ -0,0 +1,36 @@
+#include "sort.h"
+
+/**
+ * selection_sort - Trie un tableau d'entiers en ordre croissant
+ *                  avec l'algorithme de tri par sélection
+ * @array: Le tableau à trier
+ * @size: La taille du tableau
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, min_idx;
+	int tmp;
+
+	if (!array || size < 2)
+		return;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min_idx = i;
+
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min_idx])
+				min_idx = j;
+		}
+
+		if (min_idx != i)
+		{
+			tmp = array[i];
+			array[i] = array[min_idx];
+			array[min_idx] = tmp;
+
+			print_array(array, size); // Affiche l'état du tableau après chaque échange
+		}
+	}
+}
