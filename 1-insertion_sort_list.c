f47607ff355f455fc7bf64bb5f6cd396c6b1aae9
ST-GuY <estebanlvy23@gmail.com>
2025-08-08 09:20:34 +0200

    insertion

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,9 +1,9 @@
 #include "sort.h"
+
 /**
- * insertion_sort_list - A function that sorts a doubly linked list of integers
- *                      in ascending order using the insertion sort algorithm
- *
- * @list: The list containing the integers
+ * insertion_sort_list - Sorts a doubly linked list of integers
+ *                       in ascending order using insertion sort
+ * @list: Double pointer to the head of the list
  */
 void insertion_sort_list(listint_t **list)
 {
@@ -12,32 +12,39 @@ void insertion_sort_list(listint_t **list)
 	listint_t *current;
 	listint_t *prev;
 	listint_t *tmp;
-
+	/* Parcourir la liste à partir du deuxième élément */
 	while (node != NULL)
 	{
 		current = node;
 		prev = current->prev;
 		next_node = node->next;
-
+		/* Déplacer le nœud tant qu'il est plus petit que le précédent */
 		while (prev != NULL && current->n < prev->n)
 		{
-			tmp = prev->prev;
-
+			tmp = prev->prev;/* Relier tmp à current si tmp existe */
 			if (tmp != NULL)
+			{
 				tmp->next = current;
-				current->prev = tmp;
-
+			}
+			/* Mettre à jour le pointeur prev de current */
+			current->prev = tmp;/* Détacher prev de current */
 			prev->next = current->next;
+			/* Mettre à jour le prev du nœud suivant si nécessaire */
 			if (current->next != NULL)
+			{
 				current->next->prev = prev;
-			current->next = prev;
+			}
+			current->next = prev;/* Placer prev après current */
 			prev->prev = current;
-
-				if (tmp == NULL)
-					*list = current;
+			/* Si current devient la tête de liste, mettre à jour *list */
+			if (tmp == NULL)
+			{
+				*list = current; }
+			/* Continuer la comparaison vers l'arrière */
 			prev = current->prev;
-			print_list(*list);
-		}
+			/* Afficher la liste après chaque échange */
+			print_list(*list); }
+		/* Passer au nœud suivant */
 		node = next_node;
 	}
 }
